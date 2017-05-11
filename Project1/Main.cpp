c755250d5d412ec63e7c722cae46339c5fe816ff
Jianfei(Jae) Lin <lin6224@hotmail.com>
2017-05-11 01:58:45 -0400

    Add project files.

---
+++ Project1/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	// if use the namspace std, can save to write down complete name
+	std::cout << "Hello World !" << std::endl;
+	// \n is c changle new line way and same like c++ endl in the stand namespace
+	std::cout << "\n";
+	system("pause");
+	return 0;
+}
