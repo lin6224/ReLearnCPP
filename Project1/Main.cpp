eff3443a12a741a2941c14d1b37d748d19338eea
Jianfei(Jae) Lin <lin6224@hotmail.com>
2017-05-11 02:01:54 -0400

    1

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
 	// if use the namspace std, can save to write down complete name
 	std::cout << "Hello World !" << std::endl;
 	// \n is c changle new line way and same like c++ endl in the stand namespace
 	std::cout << "\n";
+
+	int x = 1;
+	cout << x << endl <<endl;
+	
+
 	system("pause");
 	return 0;
 }
